ed9b07c4440aa15020e2759950f0e11cff55f964
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-07 10:00:26 +0000

    serve index.html

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// })
+
+
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 })
 
  module.exports = app;
