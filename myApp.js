c3b0a66040012355ae326523b32c236020282db1
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-07 10:19:38 +0000

    dbg

--- myApp.js
+++ myApp.js
@@ -10,6 +10,6 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.use(__dirname + "/public", express.static() );
+app.use( express.static(__dirname + "/public") );
 
  module.exports = app;
