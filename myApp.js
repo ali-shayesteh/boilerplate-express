9724c50d635d24037e542f1bf691103c0a723d79
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-07 11:51:29 +0000

    add env

--- myApp.js
+++ myApp.js
@@ -13,7 +13,11 @@ app.get("/", function(req, res) {
 app.use( "/public", express.static(__dirname + "/public") );
 
 app.get("/json", function(req, res){
-    res.json({"message": "Hello json"});
+    const message = env.proccess.MESSAGE_STYLE === 'uppercase' 
+    ? "HELLO JSON" 
+    : "Hello json";
+
+    res.json({"message":  message});
 })
 
  module.exports = app;
