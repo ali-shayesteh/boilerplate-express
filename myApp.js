607831de34022b4d31ec39a30d490f3600641d86
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-08 06:25:49 +0000

    dbg logger

--- myApp.js
+++ myApp.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
+
+app.use(function midleware(req, res, next){
+    const log = req.method + " " + req.path + " - " + req.ip;
+    console.log(log);
+    next();
+})
+
 // app.get("/", function(req, res) {
 //     res.send("Hello Express");
 // })
@@ -20,10 +27,6 @@ app.get("/json", function(req, res){
 
 // app.use( "/public", express.static(__dirname + "/public") );
 
-app.use(function midleware(req, res, next){
-    const log = req.method + " " + req.path + " - " + req.ip;
-    console.log(log);
-    next();
-})
+
 
  module.exports = app;
