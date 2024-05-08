9d705e8ae530e7bd943eb0c9343db8dee9c925cd
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-08 05:44:45 +0000

    dbg logger

--- myApp.js
+++ myApp.js
@@ -11,13 +11,14 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.use( "/public", express.static(__dirname + "/public") );
-
 app.get("/json", function(req, res){
     res.json({"message":  process.env.MESSAGE_STYLE === 'uppercase'
     ? "HELLO JSON" 
     : "Hello json"});
 })
+
+
+app.use( "/public", express.static(__dirname + "/public") );
 
 app.use(function midleware(req, res, next){
     console.log(req.method + " " + req.path + " - " + req.ip);
