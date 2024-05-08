782b055e6630693c00fe466575f35a2019fbfc1d
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-08 07:10:55 +0000

    add /name route

--- myApp.js
+++ myApp.js
@@ -16,6 +16,11 @@ app.get("/now", function(req,res, next) {
     res.json({time: req.time})
 })
 
+app.get("/name", function (req, res) {
+    const fullName = req.query.first + " " + req.query.last;
+    res.json({ name: fullName})
+})
+
 app.get("/:word/echo", function(req, res){
     const word = req.params.word;
     res.json({echo: word})
