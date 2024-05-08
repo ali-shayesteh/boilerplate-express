42cc73454ff39012f3c2ac331d016c4dfb4d35ba
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-08 06:52:49 +0000

    echo dbg

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ app.get("/now", function(req,res, next) {
     res.json({time: req.time})
 })
 
-app.bet("/:word/echo", function(req, res){
+app.get("/:word/echo", function(req, res){
     const word = req.params.word;
     res.json({echo: word})
 })
